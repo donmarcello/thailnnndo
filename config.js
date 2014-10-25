7a0f1b969e71628a7cb7922d58c6913ffe5fb867
Your Name <you@example.com>
2014-10-25 17:55:02 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'neue',
+		wp_ws_url : 'http://www.thailando.de/app-kit-api/neue',
+		theme : 'bootstrap',
+		version : '1',
+		app_title : 'Thailando',
+		debug_mode : 'off',
+		auth_key : 'put your unique phrase here',
+		options : {"refresh_interval":0}	};
+
+});
